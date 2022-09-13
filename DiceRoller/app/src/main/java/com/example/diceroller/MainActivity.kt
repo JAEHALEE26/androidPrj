27a319994f22c427d926eae9544f7cf6ec0db2d0
JAEHALEE26 <jhl647@hanyang.ac.kr>
2022-09-13 18:39:35 +0900

    dice_final

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,21 +9,29 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
         var rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
 
     }
 
-
     private fun rollDice() {
-        val randomInt = (1..6).random()
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
-        val drawableResource = when (randomInt) {
+    private fun getRandomDiceImage(): Int {
+        val randomInt = (1..6).random()
+        return when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -31,31 +39,7 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-        diceImage.setImageResource(drawableResource)
     }
-//    var previousInt = 0
-//
-////    private fun rollDice(){
-//////        Toast.makeText(this, "button clicked",
-//////            Toast.LENGTH_SHORT).show()
-////        val resultText: TextView = findViewById(R.id.result_text)
-////        val currentInt = (1..6).random()
-////
-////        if (previousInt == currentInt){
-////            resultText.setTextColor(Color.RED)
-////            resultText.text = currentInt.toString()
-////            previousInt = currentInt
-////        }
-////
-////        else {
-////            resultText.setTextColor(Color.BLACK)
-////            resultText.text = currentInt.toString()
-////            previousInt = currentInt
-////        }
-////
-////
-////    }
 }
 
 
